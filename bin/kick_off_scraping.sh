14dad6f117c58a902848acd2e291758bbfce407b
Rogibb111 <Rogibb111@users.noreply.github.com>
2022-01-31 21:37:57 -0700

    Upgrading schedule job to throw timeout error

--- bin/kick_off_scraping.sh
+++ bin/kick_off_scraping.sh
@@ -1,3 +1,11 @@
-#!/usr/bin/env scala
+#!/usr/bin/env scala.bat
 
-scala.io.Source.fromURL("http://www.google.com")
+import java.net.{URL, HttpURLConnection}
+val connection = (new URL("http://www.google.com")).openConnection.asInstanceOf[HttpURLConnection]
+connection.setConnectTimeout(5000)
+connection.setReadTimeout(5000)
+connection.setRequestMethod("GET")
+val inputStream = connection.getInputStream
+val content = io.Source.fromInputStream(inputStream).mkString
+if (inputStream != null) inputStream.close
+println(content)
