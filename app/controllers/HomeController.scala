f59b4f072bf31846852e77f424a34f1651c96897
Robert Gibb <Rogibb111@users.noreply.github.com>
2022-01-31 22:36:45 -0700

    Setting Up Scraper Scheduler (#7)
    * Adding Bash Script for Scheduler Job to Run
    
    * Upgrading schedule job to throw timeout error
    
    * Adding Scraping Controller and Route
    
    * Updating Schedule Job with dynamic URL
    
    * Fixing formating

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,23 +4,13 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 import play.api.libs.ws._
-import net.ruippeixotog.scalascraper.browser.JsoupBrowser
-import net.ruippeixotog.scalascraper.dsl.DSL._
-import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
-import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
-import net.ruippeixotog.scalascraper.model.Element
-import play.api.libs.json.Json
-import play.api.libs.json.Writes
-
-import dao.ResortData
-import models._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents, val dao: ResortData) extends BaseController {
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
   /**
    * Create an Action to render an HTML page.
@@ -30,21 +20,7 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents, v
    * a path of `/`.
    */
   def index() = Action { implicit request: Request[AnyContent] =>
-    val browser = JsoupBrowser()
-    val doc = browser.get("http://www.arapahoebasin.com");
-    val conditionsLabels = doc >> elementList(".ab-condition_sub")
-    // Get the value for 24 Hour Snowfall
-    val dailySnowLabel = conditionsLabels.filter(el => el.text == "Past 24 Hrs")(0)
-    val dailySnowString = dailySnowLabel.siblings.find(el => el.attr("class") == "ab-condition_value").get.text
-    val dailySnowVal = dailySnowString.substring(0, dailySnowString.length()-1).toIntOption.getOrElse(0)
-    // Get the value for the height of the snow base
-    val baseLabel = conditionsLabels.filter(el => el.text == "Base")(0)
-    val baseString = baseLabel.siblings.find(el => el.attr("class") == "ab-condition_value").get.text
-    val baseVal = baseString.substring(0, baseString.length()-1).toIntOption.getOrElse(0)
 
-    
-    val databaseSnapshots: Map[Resorts, DatabaseSnapshot] = Map(ArapahoeBasin -> new DatabaseSnapshot(dailySnowVal, baseVal))
-    dao.setSnapshotForResort(databaseSnapshots)
 
     Ok(views.html.index())
   }
---
+++ bin/kick_off_scraping.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env scala.bat
+
+import java.net.{URL, HttpURLConnection}
+val url = sys.env.getOrElse("SCRAPE_URL", "http://localhost:9000/scrape")
+val connection = (new URL(url)).openConnection.asInstanceOf[HttpURLConnection]
+connection.setConnectTimeout(5000)
+connection.setReadTimeout(5000)
+connection.setRequestMethod("GET")
+val inputStream = connection.getInputStream
+val content = io.Source.fromInputStream(inputStream).mkString
+if (inputStream != null) inputStream.close
+println(content)
