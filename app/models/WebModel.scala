1ebebee1288ef5d171f1a6c4414b638ee0831750
Rogibb111 <Rogibb111@users.noreply.github.com>
2022-02-15 08:54:35 -0700

    Fixing Minor Scraping Bugs

--- app/models/WebModel.scala
+++ app/models/WebModel.scala
@@ -3,4 +3,6 @@ package models
 final case class ResortSnapshot(resort: Resorts, dailySnow: Int, baseDepth: Int)
 
 sealed trait Resorts
-case object ArapahoeBasin extends Resorts
+case object ArapahoeBasin extends Resorts {
+    override def toString: String = "Arapahoe-Basin"
+}
--- app/scrapers/ABasinScraper.scala
+++ app/scrapers/ABasinScraper.scala
@@ -1,37 +1,25 @@
 package scrapers
 
-import models.Resorts
-import models.ArapahoeBasin
-
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
 import net.ruippeixotog.scalascraper.model.Element
-import models.DatabaseSnapshot
+
+import models._
 
 class ABasinScraper extends BaseScraper(ArapahoeBasin)  {
     val conditionsLabels = browser.get("http://www.arapahoebasin.com") >> elementList(".ab-condition_sub")
 
-    private def scrape24HrSnowFall(): Int = {
+    protected override def scrape24HrSnowFall(): Int = {
         val dailySnowLabel = conditionsLabels.filter(el => el.text == "Past 24 Hrs")(0)
         val dailySnowString = dailySnowLabel.siblings.find(el => el.attr("class") == "ab-condition_value").get.text
         return dailySnowString.substring(0, dailySnowString.length()-1).toIntOption.getOrElse(0)
     }
 
-    private def scrapeBaseDepth(): Int = {
+    protected override def scrapeBaseDepth(): Int = {
         val baseLabel = conditionsLabels.filter(el => el.text == "Base")(0)
         val baseString = baseLabel.siblings.find(el => el.attr("class") == "ab-condition_value").get.text
         return baseString.substring(0, baseString.length()-1).toIntOption.getOrElse(0)
-    }
-
-    override def scrapeResort: DatabaseSnapshot = {
-        return new DatabaseSnapshot(
-            scrape24HrSnowFall(),
-            scrapeBaseDepth(),
-            scrapeTemperature(),
-            scrapeWindSpeed(),
-            scrapeCardinalDirection()
-        )
     }
 }
