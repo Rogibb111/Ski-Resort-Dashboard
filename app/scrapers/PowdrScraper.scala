cc6f30f7ae27100f5d650fd62b85c98b3affd83e
Rogibb111 <Rogibb111@users.noreply.github.com>
2022-04-07 07:58:27 -0600

    Updated Powdr Scrpr to Extend Base and Use Models

--- app/scrapers/PowdrScraper.scala
+++ app/scrapers/PowdrScraper.scala
@@ -5,21 +5,29 @@ import models.Resorts
 import scala.concurrent.{ ExecutionContext, Await }
 import scala.concurrent.duration._
 import play.api.libs.json._
+import models.PowdrResorts
 
-class PowdrScraper (ws: WSClient)(
+class PowdrScraper (ws: WSClient, resort: PowdrResorts)(
     implicit ec: ExecutionContext
-) {
+) extends BaseScraper(resort) {
     case class SnowAmount(amount: Int, duration: String)
     case class SnowResult (items:Seq[SnowAmount], location_id: Int)
 
     implicit val SnowAmountReads = Json.reads[SnowAmount]
     implicit val SnowResultReads = Json.reads[SnowResult]
 
-    private val request = ws.url("https://www.eldora.com/api/v1/dor/conditions")
+    private val request = ws.url(resort.scrapeUrl)
     private val snowReportResult: SnowResult = Await.result(request.get().map { response =>
         (response.json \ "snowReport").validate[Set[SnowResult]]
-    }, 5.seconds).get.find(report => report.location_id == 11).get
+    }, 5.seconds).get.find(report => report.location_id == resort.location_id).get
 
-    println(snowReportResult)
+    override protected def scrape24HrSnowFall(): Int = {
+        snowReportResult.items.find(item => item.duration == "24 Hours").get.amount
+    }
+    
+    override protected def scrapeBaseDepth(): Int = {
+        snowReportResult.items.find(item => item.duration == "base-depth").get.amount
+    }
+    
     
 }
