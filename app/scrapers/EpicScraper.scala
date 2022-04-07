1213d7e77d59bca775c196f9e616e2f009cf4473
Rogibb111 <Rogibb111@users.noreply.github.com>
2022-04-06 18:32:25 -0600

    Fixing Indentation

--- app/scrapers/EpicScraper.scala
+++ app/scrapers/EpicScraper.scala
@@ -16,22 +16,22 @@ case class SnowResult (Depth: SnowMeasurement, Description: String)
 class EpicScraper (ws: WSClient, resort: Resorts)(
     implicit ec: ExecutionContext
 ) extends BaseScraper(resort)  {
-  implicit val SnowMeasurementReads = Json.reads[SnowMeasurement]
-  implicit val SnowResultReads = Json.reads[SnowResult]
+    implicit val SnowMeasurementReads = Json.reads[SnowMeasurement]
+    implicit val SnowResultReads = Json.reads[SnowResult]
 
-  private val request = ws.url(resort.scrapeUrl)
-  private val snowReportResult: Set[SnowResult] = Await.result(request.get().map { response =>
-    (response.json \ "SnowReportSections").validate[Set[SnowResult]]
-  }, 5.second).get
+    private val request = ws.url(resort.scrapeUrl)
+    private val snowReportResult: Set[SnowResult] = Await.result(request.get().map { response =>
+        (response.json \ "SnowReportSections").validate[Set[SnowResult]]
+    }, 5.second).get
 
-  // val snowReport = request.get().toCompletableFuture().get().asJson.get("SnowReportSections").asInstanceOf[ArrayNode];
+    // val snowReport = request.get().toCompletableFuture().get().asJson.get("SnowReportSections").asInstanceOf[ArrayNode];
 
-  protected def scrape24HrSnowFall(): Int = {
-    return snowReportResult.find(reportItem => reportItem.Description.contains("24 Hour")).get.Depth.Inches.toInt;
-  }
-  
-  protected def scrapeBaseDepth(): Int = {
-    return snowReportResult.find(reportItem => reportItem.Description.contains("Base")).get.Depth.Inches.toInt;
-  }
+    protected def scrape24HrSnowFall(): Int = {
+        return snowReportResult.find(reportItem => reportItem.Description.contains("24 Hour")).get.Depth.Inches.toInt;
+    }
+    
+    protected def scrapeBaseDepth(): Int = {
+        return snowReportResult.find(reportItem => reportItem.Description.contains("Base")).get.Depth.Inches.toInt;
+    }
   
 }
