2ed56a78e409b5f2be30f1bef4c4ce7806cf878a
Rogibb111 <Rogibb111@users.noreply.github.com>
2022-04-09 16:29:14 -0600

    Added Error Handling and Defaults For WinterPark

--- app/scrapers/WinterParkScraper.scala
+++ app/scrapers/WinterParkScraper.scala
@@ -4,19 +4,17 @@ import play.api.libs.ws.WSClient
 import scala.concurrent.{ ExecutionContext, Await }
 import scala.concurrent.duration._
 import models.WinterPark
+import play.api.libs.json.Json
 
 class WinterParkScraper (ws: WSClient)(
 implicit ec: ExecutionContext
 ) extends BaseScraper(WinterPark) {
-
-
-
     private val request = ws.url(WinterPark.scrapeUrl)
     private val snowReportResult = Await.result(request.get().map { response => 
         (response.json \ "SnowReport" \ "MidMountainArea")
-    }, 5.second).get
+    }, 5.second).getOrElse(null)
 
-    override protected def scrape24HrSnowFall(): Int = (snowReportResult \ "Last24HoursIn").get.as[String].toFloat.toInt
+    override protected def scrape24HrSnowFall(): Int = (snowReportResult \ "Last24HoursIn").getOrElse(Json.parse("\"0\"")).as[String].toFloat.toInt
 
-    override protected def scrapeBaseDepth(): Int = (snowReportResult \ "BaseIn").get.as[String].toFloat.toInt
+    override protected def scrapeBaseDepth(): Int = (snowReportResult \ "BaseIn").getOrElse(Json.parse("\"0\"")).as[String].toFloat.toInt
 }
